99c65ad330e2bf116bdf9afefcafd3085d791854
mezeiser <mezardini@gmail.com>
2023-01-15 20:32:20 +0100

    made changes to the templates

--- tixguru/settings.py
+++ tixguru/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-h3tok7-d0^ju4tw$wa5%l$unew%hq@un-&sltds8!ztk8y&(uy'
+
 
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
