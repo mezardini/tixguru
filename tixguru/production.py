dcc3fdc13dfabc15b0a1566ebe8a33cff8850c3a
mezeiser <mezardini@gmail.com>
2023-03-14 21:15:59 +0100

    deployment in progress

--- tixguru/production.py
+++ tixguru/production.py
@@ -6,7 +6,7 @@ DEBUG = True
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['*', 'localhost']
+ALLOWED_HOSTS = ['tixvana.onrender.com', 'localhost']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
